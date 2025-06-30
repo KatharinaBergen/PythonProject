74319a3cb67268d66411d5c2b69a4f60ec572364
k <katharina.tinbergen@t-online.de>
2025-06-30 17:31:30 +0200

    factory pattern

---
+++ factory_pattern/1_Main.py
@@ -0,0 +1,5 @@
+from Factory import AnimalFactory
+
+animal = AnimalFactory.get_animal("dog")
+print(animal.speak())  # Output: Woof!
+
---
+++ factory_pattern/Factory.py
@@ -0,0 +1,17 @@
+class Dog:
+    def speak(self):
+        return "Woof!"
+
+class Cat:
+    def speak(self):
+        return "Meow!"
+
+class AnimalFactory:
+    @staticmethod
+    def get_animal(animal_type: str):
+        if animal_type == "dog":
+            return Dog()
+        elif animal_type == "cat":
+            return Cat()
+        else:
+            raise ValueError(f"Unknown animal type: {animal_type}")
